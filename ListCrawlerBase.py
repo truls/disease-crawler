f86ea7ab5eaa94fcd9c93e3eff5b47aaa9cb2f99
Henne <henne@henne-laptop.(none)>
2010-09-17 15:01:28 +0200

    ListCrawlerBase and OrphanetListCrawler initial commits

--- ListCrawlerBase.py
+++ ListCrawlerBase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-"""Name here
+"""Class-extension for crawlers
 
-Description here
+This module is used by crawlers for opening urls and impersonating a standard
+Firefox browser
 """
 
 __author__ = 'Henrik Groenholt Jensen, Truls Asheim and Michael Andersen'
@@ -12,14 +13,31 @@ __modified__='16-09-2010'
 import urllib2
 
 class ListCrawlerBase(object):
-    """Description here"""
+    """So far only used for opening urls"""
+
+    def __init__(self,query):
+        """Nothing to initiate"""
+        pass
 
     def open_url(self, url):
-        """specifics here"""
+        """
+        @param url: url used to access the specific page
+        @type url: str
 
+        @return: Returns a handle for the specific url
+        @rtype: instance of urllib2.urlopen()
+
+        @raise URLError: If the page is not recognized
+        """
+    
         request = urllib2.Request(url)
         request.add_header("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT "
             "6.1; he; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8")
         request.add_header("Referer", "http://www.google.com")
-
-        return urllib2.urlopen(request)
+    
+        try:
+            print type(urllib2.urlopen(request))
+            return urllib2.urlopen(request)
+        except Exception as e:
+            print "Could not open url"
+            raise e
---
+++ OrphanetListCrawler.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+"""Module for retrieving diseases from Orphanet
+
+Retrieves the initial list of diseases and urls to their description on 
+Orphanet
+"""
+
+__author__ = 'Henrik Groenholt Jensen, Truls Asheim and Michael Andersen'
+__version__= '1.0'
+__modified__='16-09-2010'
+
+from ListCrawlerBase import ListCrawlerBase
+import lxml
+import lxml.html
+from lxml import etree
+
+# should be moved to the controller module:
+import time
+import random
+
+class OrphanetListCrawler(ListCrawlerBase):
+        
+    orphanet_url = 'http://www.orpha.net/consor/cgi-bin/Disease_Search_List.php?lng=EN&TAG='
+
+    def __init__(self, query=None):
+        """Nothing to initiate"""
+        self.query = query
+
+    def get_results(self):
+        """Get a tubled list of disease urls and names
+
+        In short: Go through all the disease categories ('A','B','C'...) using
+        ListCrawlerBase and parse the pages with lxml
+
+        @return: List of tubles on the form [(url,name)]
+        @rtype: list of tubles 
+        """
+
+        results=[]
+
+        pages = lambda: ['0'] + [chr(x) for x in range(65,90)]
+        pages_url_base = "http://www.orpha.net/consor/cgi-bin/"
+
+        for page in pages():
+            result=None
+
+            random.seed(time.time())
+            time.sleep(random.randrange(3, 6))
+
+            category_url = self.orphanet_url + page
+
+            html = self.open_url(category_url)
+
+            parser = etree.HTMLParser()
+            tree = lxml.etree.parse(html, parser)
+            specific_urls = tree.xpath("//div[@id='result-box']/ul/li/a")
+            
+            result=[(pages_url_base+url.get("href"),url.text) for url in specific_urls]
+            
+            results.extend(result)
+
+            print "Current category:",page,"- retrived",len(result),"urls"
+            print "In total:",len(results),"urls\n"
+
+        return results
+
+
+
